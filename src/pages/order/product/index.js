bf91de6bb47331b6164eb7898d2c006971b7d700
Lawal Habeebullahi <lekanlawal2025@gmail.com>
2023-02-06 03:18:56 +0100

    Delete order

--- src/pages/order/product/index.js
+++ src/pages/order/product/index.js
@@ -1,8 +1,13 @@
-// import axios from "axios";
+import axios from "axios";
 import { useLocation, Link } from "react-router-dom";
-
+import { useContext, useState } from "react";
+import { AuthContext } from "../../../context/AuthContext";
+// const url = "http://localhost:4000/order_items?limit=5";
+const url = "https://unshelled-api.cyclic.app/order_items/";
 const Product = ({ props }) => {
   const { state } = useLocation();
+  const { auth } = useContext(AuthContext);
+  const [delRes, setDelRes] = useState(null);
   // const { id } = useParams();
   return (
     <>
@@ -13,12 +18,28 @@ const Product = ({ props }) => {
         Edit this product
       </Link>
       <button
-        onClick={() => {
-          // axios.delete()
+        onClick={async () => {
+          await axios
+            .delete(`${url}${state.id}`, {
+              auth: {
+                username: auth.username,
+                password: auth.password,
+              },
+            })
+            .then((d) => {
+              // return <p>{d.data}</p>;
+              // console.log(d);
+              setDelRes(d.data);
+            })
+            .catch((e) => {
+              console.log(e);
+            });
         }}
       >
         Delete this product.
       </button>
+      <p>{delRes}</p>
+      <Link to={"/order"}>Back to orders page</Link>
     </>
   );
 };
